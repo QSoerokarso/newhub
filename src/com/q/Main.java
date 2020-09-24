230057dcb778ff3b19f881c0ae81de4ef993dc85
eghbol <eqhbal@gmail.com>
2020-09-24 20:46:28 +0200

    Eq commit

--- src/com/q/Main.java
+++ src/com/q/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("lekker krabbelen op de printline");
                System.out.println("Say no more !!!!!");
+        System.out.println("CAVIAAR");
 
     }
 }
